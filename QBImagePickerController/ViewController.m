af2ab12b8045ba4698bba7dde2b58f01a17320ca
JYLee <duqwo06@naver.com>
2018-07-13 17:33:15 +0900

    커스텀 사진첩 기능 테스트

--- QBImagePickerController/ViewController.m
+++ QBImagePickerController/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import <QBImagePickerController/QBImagePickerController.h>
 
-@interface ViewController ()
+@interface ViewController () <QBImagePickerControllerDelegate>
 
 @end
 
@@ -19,11 +20,83 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (IBAction)goAssetView:(id)sender {
+    [self startAssetView];
+}
+
+- (void) startAssetView {
+    QBImagePickerController *imagePickerController = [QBImagePickerController new];
+    imagePickerController.delegate = self;
+    imagePickerController.mediaType = QBImagePickerMediaTypeAny;
+    imagePickerController.allowsMultipleSelection = YES;
+    imagePickerController.showsNumberOfSelectedAssets = YES;
+    
+    imagePickerController.maximumNumberOfSelection = 3;
+    
+    [self presentViewController:imagePickerController animated:YES completion:NULL];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+
+#pragma mark - QBImagePickerControllerDelegate
+
+- (void)qb_imagePickerController:(QBImagePickerController *)imagePickerController didFinishPickingAssets:(NSArray *)assets
+{
+    NSLog(@"Selected assets:");
+    NSLog(@"%@", assets);
+    
+    
+    
+    PHImageRequestOptions *requestOptions = [[PHImageRequestOptions alloc] init];
+    requestOptions.resizeMode   = PHImageRequestOptionsResizeModeExact;
+    requestOptions.deliveryMode = PHImageRequestOptionsDeliveryModeHighQualityFormat;
+    
+    // this one is key
+    requestOptions.synchronous = YES;
+    
+    NSMutableArray *assetsMutableArray = [NSMutableArray arrayWithArray:assets];
+    PHImageManager *manager = [PHImageManager defaultManager];
+    NSMutableArray *images = [NSMutableArray arrayWithCapacity:[assets count]];
+    
+    // assets contains PHAsset objects.
+    __block UIImage *ima;
+    
+    for (PHAsset *asset in assetsMutableArray) {
+        // Do something with the asset
+        
+        [manager requestImageForAsset:asset
+                           targetSize:PHImageManagerMaximumSize
+                          contentMode:PHImageContentModeDefault
+                              options:requestOptions
+                        resultHandler:^void(UIImage *image, NSDictionary *info) {
+                            ima = image;
+                            
+                            NSLog(@"select assets image : %@",ima);
+                            
+                            [images addObject:ima];
+                            
+                        }];
+    }
+    
+    NSLog(@"select assets image array : %@",images);
+    
+    UIImageView *testImageView = [[UIImageView alloc] initWithFrame:self.view.frame];
+    [testImageView setImage:[images firstObject]];
+    [testImageView setBackgroundColor:[UIColor redColor]];
+    [self.view addSubview:testImageView];
+    
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+- (void)qb_imagePickerControllerDidCancel:(QBImagePickerController *)imagePickerController
+{
+    NSLog(@"Canceled.");
+    
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
 
 @end
