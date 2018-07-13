bc4b82d5554400768a346de02d7204a2dde20c9e
JYLee <duqwo06@naver.com>
2018-07-13 16:00:15 +0900

    Initial Commit

---
+++ QBImagePickerController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QBImagePickerController
+//
+//  Created by SKOINFO_MACBOOK on 2018. 7. 13..
+//  Copyright © 2018년 SKOINFO_MACBOOK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QBImagePickerController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QBImagePickerController
+//
+//  Created by SKOINFO_MACBOOK on 2018. 7. 13..
+//  Copyright © 2018년 SKOINFO_MACBOOK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
